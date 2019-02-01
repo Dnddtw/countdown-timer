d120c851faba2553bc4d8e0ee3282b776847ca40
Sloboda Andriy <sloboda.a@d2.digital>
2019-02-01 14:40:37 +0200

    Updates

---
+++ precache-manifest.0f3dbe87b57205642a3c8812ab8de4f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "149e0cbf04c96f2aa65c",
+    "url": "/countdown-timer/static/js/runtime~main.149e0cbf.js"
+  },
+  {
+    "revision": "84d9757b8167bf6fc758",
+    "url": "/countdown-timer/static/js/main.84d9757b.chunk.js"
+  },
+  {
+    "revision": "c5e9f0bb97e803a7eb2a",
+    "url": "/countdown-timer/static/js/1.c5e9f0bb.chunk.js"
+  },
+  {
+    "revision": "84d9757b8167bf6fc758",
+    "url": "/countdown-timer/static/css/main.1007a0d8.chunk.css"
+  },
+  {
+    "revision": "2a457b1736ba8bc0aa033a8960504d61",
+    "url": "/countdown-timer/index.html"
+  }
+];
